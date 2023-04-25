0646b1299f9797cb6af829ea6dc16da406fbed85
Nalinaka <nalinaka@gmail.com>
2023-04-24 21:31:47 -0700

    Completed with a lot of solution assistance, but a nice code along to see how its done.

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,52 @@
 
-// Your Code Here
+//let response = await fetch ('https://localhost:9001/updateBook', {
+  //  let books = await response.json(),
+   // method: "PATCH",
+    //headers: {
+      //  'Content-Type': 'application/json'
+    //},
+   // body: JSON.stringify({ only used when submitting data
+   //     "id": 3,
+    //    "title": "Legends of Arathrae",
+  //  }),
+//});
+
+//let updateBook = await response.json();
+//console.log(updatedBook)
+
+
+async function main() {
+ let response = await fetch ('http://localhost:3001/listBooks')
+ let books = await response.json()
+ books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.queryselector('#root')
+    
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('input')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH'
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        body: JSON.stringify({
+            id: book.id,
+            quantity: quantityInput.value 
+        })
+        })
+    })
+    li.append(quantityInput, saveButton)
+    root.append (li)
+}
+
+main();
